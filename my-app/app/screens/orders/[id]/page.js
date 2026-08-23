21a8402a07fc053821c6a22725043ee73f7be754
legic <justpetk@pm.me>
2026-08-23 18:22:38 +0300

    minor change for order details page

--- my-app/app/screens/orders/[id]/page.js
+++ my-app/app/screens/orders/[id]/page.js
@@ -1,8 +1,55 @@
-export default async function orderList({params}) {
-  const { id } = await params;
+"use client";
+import { supabase } from "./../../../supabaseClient"; // Importuojame klientą
+import { use, useState, useEffect } from "react";
+
+export default function orderList({ params }) {
+  const { id } = use(params);
+  const [car, setCar] = useState(null);
+  const [customer, setCustomer] = useState(null);
+
+  useEffect(() => {
+    fetchOrderDetails();
+  }, []);
+
+  const fetchOrderDetails = async () => {
+    const { data, error } = await supabase
+      .from("orders")
+      .select(
+        `
+    id,
+    created_at,
+    total_price,
+    income,
+    status,
+    car:cars (
+      id,
+      car_name,
+      registration_no,
+      year,
+      customer:customers (
+        id,
+        name,
+        phone_number,
+        creation_date
+      )
+    )
+  `,
+      )
+      .eq("id", id)
+      .single();
+
+    if (error) {
+      console.log(error);
+    } else {
+      setCar(data.car);
+      setCustomer(data.car.customer);
+    }
+  };
+
   return (
     <div style={{ padding: "30px", fontFamily: "Arial" }}>
       <h1>Order List {id}</h1>
+      <button onClick={() => console.log(car, customer)}>Log Details</button>
     </div>
   );
 }
